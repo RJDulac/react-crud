b4685815ce469ee88d7da52e04e499e634a28f80
Ryan Dulac <analyticrye@gmail.com>
2018-06-20 02:11:13 -0400

    todo adding functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       super();
 
       this.state = {
+        newTodo: '',
+
         todos: [{
           id: 1, name: 'Play Sims 4'
         }, {
@@ -19,7 +21,40 @@ class App extends Component {
         }]
       };
     }
+
+handleChange = (event) => {
+  // console.log(event.target.name, event.target.value);
+  this.setState({
+    newTodo: event.target.value
+
+  });
+
+  //two way binding if you don't use arrow function
+  // this.handleChange = this.handleChange.bind(this);
+
+
+}
+
+addTodo = () => {
+  const newTodo = {
+    name: this.state.newTodo,
+    //get the last id by subtracting and add the next id with + operator
+    id: this.state.todos[this.state.todos.length -1].id + 1
+  };
+
+  const todos = this.state.todos;
+
+  todos.push(newTodo);
+
+  this.setState({
+    todos: todos,
+    //clear after clicking button
+    newTodo: ''
+  });
+}
+
   render() {
+    console.log(this.state.newTodo);
     return (
       <div className="App">
         <header className="App-header">
@@ -28,7 +63,18 @@ class App extends Component {
         </header>
 
         <div className="container">
-          <h2 className="text-center p4">Todos App</h2>
+          <input 
+            type='text'
+            name='todo' 
+            className="form-control my-4"
+            placeholder="Add a new todo"
+            onChange={this.handleChange}
+            value={this.state.newTodo}
+          />
+          <button
+            onClick={this.addTodo}
+            disabled={this.state.newTodo.length === 0} 
+            className="btn-info mb-3 form-control">Add Todo</button>
           <ul className="list-group">
             {this.state.todos.map((item) => {
 
